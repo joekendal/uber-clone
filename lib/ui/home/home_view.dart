ffe91ce1706cf3aeab3ee35defee1de73a304cf6
joekendal <13680617+joekendal@users.noreply.github.com>
2020-06-28 04:19:36 +0100

    Added public credentials

--- lib/ui/home/home_view.dart
+++ lib/ui/home/home_view.dart
@@ -1,9 +1,18 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:uber/datamodels/user_location.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Completer<GoogleMapController> _controller = Completer();
+
   @override
   Widget build(BuildContext context) {
     var userLocation = Provider.of<UserLocation>(context);
@@ -11,12 +20,16 @@ class HomePage extends StatelessWidget {
     var cameraPosition = userLocation != null
         ? CameraPosition(
             target: LatLng(userLocation.latitude, userLocation.longitude),
+            zoom: 13,
           )
         : null;
     return Scaffold(
       body: cameraPosition != null
           ? GoogleMap(
               initialCameraPosition: cameraPosition,
+              onMapCreated: (GoogleMapController controller) {
+                _controller.complete(controller);
+              },
             )
           : Text(""),
     );
